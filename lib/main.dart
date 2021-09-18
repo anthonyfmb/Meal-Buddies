74d6fb2cf01f69a69b7a1678e1a2a45302a441c5
committ <66704935+committ@users.noreply.github.com>
2021-09-18 14:18:48 -0400

    Authentication Changes/Matching page
    Added
    - Matching page
    Modified
    - log_in
    - sign_up
    - main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:meal_buddies/Screens/Welcome/log_in.dart';
 
 import 'Screens/Welcome/sign_up.dart';
 
+/*
+Author: David, Anthony
+Date: 9/18/2021
+This class is responsible for being the main method of the whole project.
+
+*/
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
