98508e31b9dda3f4d13969fc77117bc5206a17d5
committ <66704935+committ@users.noreply.github.com>
2021-09-19 04:51:13 -0400

    Tiny Bug

--- lib/Screens/Matching/choose_court.dart
+++ lib/Screens/Matching/choose_court.dart
@@ -65,7 +65,7 @@ class _HomePageState extends State<HomePage> {
   AppBar buildAppBar() {
     return AppBar(
       centerTitle: true,
-      title: Text(Constants.name.toString()),
+      title: Text(""),
       actions: <Widget>[
         IconButton(
             icon: Icon(icon),
